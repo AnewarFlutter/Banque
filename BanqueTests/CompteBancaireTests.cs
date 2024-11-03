99b11447e626e568b032c439d2ba32d3f5780fa8
El Anewar <dmlattanda@gmail.com>
2024-11-03 17:59:02 +0000

    Ajoutez des fichiers projet.

---
+++ BanqueTests/CompteBancaireTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using CompteBanqueNS;
+
+namespace BanqueTests
+{
+    [TestClass]
+    public class CompteBancaireTests
+    {
+        [TestMethod]
+        public void VérifierDébitCompteCorrect()
+        {
+            // Ouvrir un compte
+            double soldeInitial = 500000;
+            double montantDébit = 400000;
+            double soldeAttendu = 100000;
+            var compte = new CompteBancaire("Pr. Abdoulaye Diankha", soldeInitial);
+
+            // Débiter
+            compte.Débiter(montantDébit);
+
+            // Tester
+            double soldeObtenu = compte.Balance;
+            Assert.AreEqual(soldeAttendu, soldeObtenu, 0.001, "Compte débité incorrectement");
+        }
+    }
+}
---
+++ BanqueTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("BanqueTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BanqueTests")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("51b332cb-57a9-4597-a494-7a8050b6b011")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
